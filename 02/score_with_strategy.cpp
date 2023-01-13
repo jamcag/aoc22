1ecb0b66b3f61ee073e67023c61caf6aeb087cfb
JC <jamcag@proton.me>
2023-01-13 12:42:31 -0500

    Do day 2 part 2 problem

---
+++ 02/score_with_strategy.cpp
@@ -0,0 +1,40 @@
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+#include <sstream>
+
+using std::cout;
+using std::getline;
+using std::ifstream;
+using std::istringstream;
+using std::map;
+using std::string;
+
+constexpr int ROCK_SCORE = 1;
+constexpr int PAPER_SCORE = 2;
+constexpr int SCISSORS_SCORE = 3;
+constexpr int LOSS_SCORE = 0;
+constexpr int DRAW_SCORE = 3;
+constexpr int WIN_SCORE = 6;
+
+int main() {
+    const map<char, map<char, int>> scores{
+        {'A', {{'X', SCISSORS_SCORE + LOSS_SCORE}, {'Y', ROCK_SCORE + DRAW_SCORE}, {'Z', PAPER_SCORE + WIN_SCORE}}},
+        {'B', {{'X', ROCK_SCORE + LOSS_SCORE}, {'Y', PAPER_SCORE + DRAW_SCORE}, {'Z', SCISSORS_SCORE + WIN_SCORE}}},
+        {'C', {{'X', PAPER_SCORE + LOSS_SCORE}, {'Y', SCISSORS_SCORE + DRAW_SCORE}, {'Z', ROCK_SCORE + WIN_SCORE}}},
+    };
+
+    ifstream ifs("input");
+    string s;
+    int total = 0;
+    while (getline(ifs, s)) {
+        istringstream iss(s);
+        char opponent;
+        iss >> opponent;
+        char mine;
+        iss >> mine;
+        total += scores.at(opponent).at(mine);
+    }
+    cout << total << "\n";
+}
