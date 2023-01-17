fa9844059bdbb41e5421dfa5a0d0f475383e7cae
JC <jamcag@proton.me>
2023-01-17 12:30:31 -0500

    Do day 6 problems

---
+++ 06/marker_14.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <set>
+
+int main() {
+    std::ifstream ifs{"../input"};
+    std::string s;
+    ifs >> s;
+
+    for (int i = 13; i < s.size(); ++i) {
+        std::set<char> last_fourteen_unique;
+        for (int j = i-13; j <= i; ++j) {
+            last_fourteen_unique.emplace(s.at(j));
+        }
+        if (last_fourteen_unique.size() == 14) {
+            std::cout << i+1 << "\n";
+            return EXIT_SUCCESS;
+        }
+    }
+    return 0;
+}
---
+++ 06/marker_4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <set>
+
+int main() {
+    std::ifstream ifs{"../input"};
+    std::string s;
+    ifs >> s;
+
+    for (int i = 3; i < s.size(); ++i) {
+        std::set<char> last_four_unique;
+        for (int j = i-3; j <= i; ++j) {
+            last_four_unique.emplace(s.at(j));
+        }
+        if (last_four_unique.size() == 4) {
+            std::cout << i+1 << "\n";
+            return EXIT_SUCCESS;
+        }
+    }
+    return 0;
+}
