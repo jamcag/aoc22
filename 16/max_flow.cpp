211803a1bd161c3f3f7fc5a6b614ea1bd69a617a
JC <jamcag@proton.me>
2023-02-04 18:22:54 -0500

    Fix BFS to use queue instead of stack

--- 16/max_flow.cpp
+++ 16/max_flow.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <deque>
 #include <fstream>
 #include <iostream>
 #include <map>
@@ -31,30 +32,27 @@ public:
     World(std::map<std::string, Valve> valves, std::vector<std::string> positive_rate_valves) : valves_{valves}, positive_rate_valves_{positive_rate_valves} {}
     CostToOpen get_costs_to_open(State state) {
         std::string start = state.current;
-        if (start == "JJ") {
-            int dbg = 1;
-        }
-        if (!cost_cache_.contains(start)) {
+        if (!cost_cache_.count(start) > 0) {
             std::map<std::string, int> costs;
             // Find the shortest paths to every non-zero node.
-            std::stack<std::vector<std::string>> frontier;
+            std::queue<std::vector<std::string>> frontier;
             std::set<std::string> visited;
             frontier.emplace(std::vector<std::string>{start});
             while (!frontier.empty()) {
-                auto cur_path = frontier.top();
+                auto cur_path = frontier.front();
                 auto cur = cur_path.back();
                 frontier.pop();
+                visited.emplace(cur);
                 if (std::find(positive_rate_valves_.begin(), positive_rate_valves_.end(), cur) != positive_rate_valves_.end()) {
-                    costs[cur] = cur_path.size();
-                    std::cout << "\tstart=" << start << ", end=" << cur << ", cost=" << costs[cur];
-                    std::cout << ", path=";
-                    for (const auto valve : cur_path) {
-                        std::cout << valve;
+                    std::cout << "start=" << cur_path[0] << ", end=" << cur << ", cost=" << cur_path.size() << "\n";
+                    if (cur_path[0] == cur && cur_path.size() != 1) {
+                        throw;
                     }
-                    std::cout << "\n";
+                    costs[cur] = cur_path.size();
                 }
                 for (const auto neighbour : valves_[cur].neighbours) {
-                    if (!visited.contains(neighbour)) {
+                    const bool visited_contains_neighbour = visited.count(neighbour) > 0;
+                    if (!visited_contains_neighbour) {
                         auto new_path = cur_path;
                         new_path.emplace_back(neighbour);
                         frontier.emplace(new_path);
@@ -69,16 +67,50 @@ public:
             costs.erase(open_valve);
         }
 
-        for (const auto [k, v] : costs) {
-            std::cout << "\tk=" << k << ", v=" << v << "\n";
-        }
         return costs;
+    }
+
+    int get_rate(std::string valve) const {
+        return valves_.at(valve).rate;
     }
 private:
     std::map<std::string, Valve> valves_;
     std::vector<std::string> positive_rate_valves_;
     std::map<std::string, CostToOpen> cost_cache_;
 };
+
+void valves_to_graphviz(const std::map<std::string, Valve>& valves) {
+    std::ofstream ofs{"all_valves.dot"};
+    ofs << "digraph G {";
+    for (const auto [k, v] : valves) {
+        ofs << "\t" << k << "[label=\"" << k << "\\n" << v.rate << "\"];\n";
+    }
+    // Print neighbours.
+    for (const auto [k, v] : valves) {
+        for (const auto neighbour : v.neighbours) {
+            ofs << "\t" << k << " -> " << neighbour << ";\n";
+        }
+    }
+    ofs << "}";
+}
+
+void important_valves_to_graphviz(const std::vector<std::string>& positive_rate_valves, World& world) {
+    std::ofstream ofs{"important_valves.dot"};
+    ofs << "digraph G {\n";
+    for (const auto valve : positive_rate_valves) {
+        ofs << "\t" << valve << "[label=\"" << valve << "\\n" << world.get_rate(valve) << "\"];\n";
+    }
+
+    for (const auto valve : positive_rate_valves) {
+        State s;
+        s.current = valve;
+        for (const auto [neighbour, cost] : world.get_costs_to_open(s)) {
+            ofs << "\t" << valve << " -> " << neighbour << " [label=\"" << cost << "\"];\n";
+        }
+    }
+    ofs << "}";
+}
+
 int main() {
     std::ifstream ifs{"input"};
     std::string s;
@@ -115,15 +147,10 @@ int main() {
             }
             valve.neighbours.emplace_back(other_valve);
         }
-
-        std::cout << "valve.name=" << valve.name << ", valve.rate=" << valve.rate << ", valve.neighbors: ";
-        for (const auto neighbor : valve.neighbours) {
-            std::cout << neighbor << ", ";
-        }
-        std::cout << "\n";
     }
-    std::cout << "\n";
+    valves_to_graphviz(valves);
     auto world = World {valves, positive_rate_valves};
+    important_valves_to_graphviz(positive_rate_valves, world);
     State initial;
     initial.current = "AA";
     initial.time_remaining = kTimesteps;
@@ -137,10 +164,6 @@ int main() {
         auto cur_state = path.back();
 
         frontier.pop();
-        std::cout << "cur_state.current=" << cur_state.current << "\n";
-        std::cout << "cur_state.total_flow=" << cur_state.total_flow << "\n";
-        std::cout << "cur_state.flow_per_minute=" << cur_state.flow_per_minute << "\n";
-        std::cout << "cur_state.time_remaining=" << cur_state.time_remaining << "\n";
         std::cout << "path=";
         for (const auto s : path) { std::cout << s.current;}
         std::cout << "\n";
@@ -148,10 +171,7 @@ int main() {
         // Find the path to all closed valves with rate > 0 using BFS.
         CostToOpen costs = world.get_costs_to_open(cur_state);
         if (costs.empty()) {
-            std::cout << "\tempty\n";
-            std::cout << "\ttotal=std::max(cur_state.total_flow + cur_state.flow_per_minute * cur_state.time_remaining=" << cur_state.total_flow + cur_state.flow_per_minute * cur_state.time_remaining << ", total=" << total << ")=";
             total = std::max(cur_state.total_flow + cur_state.flow_per_minute * cur_state.time_remaining, total);
-            std::cout << total << "\n";
         }
         for (const auto [k, v] : costs) {
             State next = cur_state;
@@ -168,20 +188,10 @@ int main() {
                 frontier.emplace(next_path);
 
             } else {
-                std::cout << "\telse";
-                std::cout <<" cur_state.flow_per_minute=" << cur_state.flow_per_minute << ", cur_state.time_remaining=" << cur_state.time_remaining << "\n";
                 next.total_flow += cur_state.flow_per_minute * cur_state.time_remaining;
             }
-            std::cout << "\t\ttotal=std::max(next.total_flow=" << next.total_flow << ", total=" << total << ")=";
             total = std::max(total, next.total_flow);
-            std::cout << total << "\n";
-            std::cout << "\tnext.current=" << next.current << "\n";
-            std::cout << "\t\tnext.time_remaining=" << next.time_remaining << "\n";
-            std::cout << "\t\tnext.total_flow=" << next.total_flow << "\n";
-            std::cout << "\t\tnext.flow_per_minute=" << next.flow_per_minute << "\n";
         }
-        std::cout << "\ttotal=" << total << "\n";
-        std::cout << "\tfrontier.size()=" << frontier.size() << "\n";
     }
 
     std::cout << total << "\n";
