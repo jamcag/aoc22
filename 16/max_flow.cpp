9b588995c0e3a5b874143de00fc35d5365723f97
JC <jamcag@proton.me>
2023-02-04 01:21:00 -0500

    Implement cost-to-open

--- 16/max_flow.cpp
+++ 16/max_flow.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <map>
@@ -5,23 +6,77 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <stack>
+#include <set>
+
+int const kTimesteps = 7;
 
 struct Valve {
     std::string name;
     std::vector<std::string> neighbours;
     int rate;
-    bool open = false;
 };
 
-bool operator==(const Valve& lhs, const std::string& rhs) {
-    return lhs.name == rhs;
-}
+struct State {
+    std::string current;
+    std::vector<std::string> open;
+    int time_remaining; 
+    int flow_per_minute = 0;
+    int total_flow = 0;
+};
 
+using CostToOpen = std::map<std::string, int>;
+class World {
+public:
+    World(std::map<std::string, Valve> valves, std::vector<std::string> positive_rate_valves) : valves_{valves}, positive_rate_valves_{positive_rate_valves} {}
+    CostToOpen get_costs_to_open(State state) {
+        std::string start = state.current;
+        if (!cost_cache_.contains(start)) {
+            std::map<std::string, int> costs;
+            // Find the shortest paths to every non-zero node.
+            std::stack<std::vector<std::string>> frontier;
+            std::set<std::string> visited;
+            frontier.emplace(std::vector<std::string>{start});
+            while (!frontier.empty()) {
+                auto cur_path = frontier.top();
+                auto cur = cur_path.back();
+                visited.emplace(cur);
+                frontier.pop();
+                if (std::find(positive_rate_valves_.begin(), positive_rate_valves_.end(), cur) != positive_rate_valves_.end()) {
+                    costs[cur] = cur_path.size();
+                }
+                for (const auto neighbour : valves_[cur].neighbours) {
+                    if (!visited.contains(neighbour)) {
+                        auto new_path = cur_path;
+                        new_path.emplace_back(neighbour);
+                        frontier.emplace(new_path);
+                    }
+
+                }
+            }
+            cost_cache_[start] = costs;
+        }
+        auto costs = cost_cache_[start];
+        for (const auto open_valve : state.open) {
+            costs.erase(open_valve);
+        }
+
+        for (const auto [k, v] : costs) {
+            std::cout << "\tk=" << k << ", v=" << v << "\n";
+        }
+        return costs;
+    }
+private:
+    std::map<std::string, Valve> valves_;
+    std::vector<std::string> positive_rate_valves_;
+    std::map<std::string, CostToOpen> cost_cache_;
+};
 int main() {
     std::ifstream ifs{"input"};
     std::string s;
 
     std::map<std::string, Valve> valves;
+    std::vector<std::string> positive_rate_valves;
     while (std::getline(ifs, s)) {
         std::string _;
         std::string name;
@@ -41,7 +96,9 @@ int main() {
         auto& valve = valves[name];
         valve.name = name;
         valve.rate = std::stoi(rate.substr(5, rate.size() - 2));
-
+        if (valve.rate > 0) {
+            positive_rate_valves.emplace_back(name);
+        }
         iss >> tunnels >> lead >> to >> __;
 
         while (iss >> other_valve) {
@@ -57,65 +114,42 @@ int main() {
         }
         std::cout << "\n";
     }
+    std::cout << "\n";
+    auto world = World {valves, positive_rate_valves};
+    State initial;
+    initial.current = "AA";
+    initial.time_remaining = kTimesteps;
+    initial.total_flow = 0;
 
-    // Find benefit over time and cost-to-activate
-    // Benefit is rate * (num_steps_left - (distance + 1))
-    std::string last = "AA";
-    int minutes_remaining = 30;
+    std::queue<State> frontier;
+    frontier.emplace(initial);
     int total = 0;
+    while (!frontier.empty()) {
+        auto cur_state = frontier.front();
+        frontier.pop();
+        std::cout << "cur_state.current=" << cur_state.current << "\n";
+        // Find the path to all closed valves with rate > 0 using BFS.
+        CostToOpen costs = world.get_costs_to_open(cur_state);
 
-    while (minutes_remaining > 0) {
-        std::queue<std::vector<std::string>> frontier;
-        frontier.emplace(std::vector<std::string>{last});
-        std::map<std::string, int> cost;
-        std::map<std::string, int> reward;
-        while (!frontier.empty()) {
-            const auto path = frontier.front();
-            frontier.pop();
-            const auto tail_name = path[path.size() - 1];
-            const auto tail = valves[tail_name];
-            if (cost.contains(tail.name)) {
-                continue;
-            }
+        for (const auto [k, v] : costs) {
+            State next = cur_state;
+            next.current = k;
+            next.open = cur_state.open;
+            next.open.emplace_back(cur_state.current);
+            next.time_remaining -= (v);
 
-            cost[tail.name] = path.size();
-            const auto &valve = valves[tail.name];
-            if (tail.open) {
-                reward[tail.name] = 0;
-            } else {
+            frontier.emplace(next);
+            next.total_flow += cur_state.flow_per_minute * (v);
+            next.flow_per_minute += valves[k].rate;
 
-                reward[tail.name] = (minutes_remaining - (cost[tail.name])) * valve.rate;
-            }
-
-            for (const auto &neighbour: valve.neighbours) {
-                if (cost.contains(neighbour)) {
-                    continue;
-                }
-                if (std::find(path.begin(), path.end(), neighbour) != path.end()) {
-                    continue;
-                }
-                std::vector new_path = path;
-                new_path.emplace_back(neighbour);
-                frontier.emplace(new_path);
-            }
+            std::cout << "\tnext.current=" << next.current << "\n";
+            std::cout << "\t\tnext.time_remaining=" << next.time_remaining << "\n";
+            std::cout << "\t\tnext.total_flow=" << next.total_flow << "\n";
+            std::cout << "\t\tnext.flow_per_minute=" << next.flow_per_minute << "\n";
+            total = std::max(total, next.total_flow);
         }
-
-        int max = -1;
-        std::string best = "";
-        for (const auto [k, v] : valves) {
-            std::cout << v.name << ": cost=" << cost[k] << ", reward=" << reward[k] << "\n";
-            if (reward[k] > max) {
-                max = reward[k];
-                best = k;
-            }
-        }
-        last = best;
-        std::cout << "Going to " << best << " for reward " << max << "\n";
-        minutes_remaining -= cost[best];
-        std::cout << "Time remaining " << minutes_remaining << "\n";
-        total += max;
-        valves[best].open = true;
-        std::cout << "\n";
+        std::cout << "\tfrontier.size()=" << frontier.size() << "\n";
     }
+
     std::cout << total << "\n";
 }
