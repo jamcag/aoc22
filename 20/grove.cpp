515c6225ee5f9586bd312ca0b370113fdc5645a3
JC <jamcag@proton.me>
2023-02-15 02:28:29 -0500

    Start doing day 20

---
+++ 20/grove.cpp
@@ -0,0 +1,31 @@
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <vector>
+
+using std::vector;
+
+class Mover {
+public:
+  Mover(vector<int> data) : data_{data} {
+    for (size_t i = 0; i < data_.size(); ++i) {
+      indices_.emplace_back(i);
+    }
+  }
+
+  vector<int> render() {
+    return data_;
+  }
+private:
+  vector<int> data_;
+  vector<size_t> indices_;
+};
+
+int main() {
+  std::ifstream ifs{"input"};
+  int n;
+  vector<int> nums;    
+  while (ifs >> n) {
+    nums.emplace_back(n);
+  }
+}
